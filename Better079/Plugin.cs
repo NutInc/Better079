990177cc0fd9854c0fc7fa6d66cecab2625e5206
Build <jwbradley2003@gmail.com>
2022-02-28 18:41:15 -0500

    Bump Version

--- Better079/Plugin.cs
+++ Better079/Plugin.cs
@@ -34,9 +34,6 @@ namespace Better079
         /// <inheritdoc/>
         public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
-        /// <inheritdoc />
-        public override Version Version { get; } = new Version(2, 0, 0);
-
         /// <inheritdoc/>
         public override void OnEnabled()
         {
